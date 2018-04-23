e0b9b8d9984ee906aa646082b4e6a83f4c180ba2
Mateusz Bednarski <echelon133@gmail.com>
2018-04-23 14:56:38 +0200

    Added User model

---
+++ src/main/java/ml/echelon133/Model/User.java
@@ -0,0 +1,101 @@
+package ml.echelon133.Model;
+
+import ml.echelon133.Security.SecretGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String secret;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name="user_authorities",
+            joinColumns = @JoinColumn(name="user_fk", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name="authority_fk", referencedColumnName = "id")
+    )
+    private Set<Authority> authorities;
+
+    @OneToMany(mappedBy="listOwner", cascade = CascadeType.PERSIST)
+    private List<TodoList> todoLists;
+
+    public User() {
+        setTodoLists(new ArrayList<>());
+        setAuthorities(new HashSet<>());
+
+        SecretGenerator sgen = new SecretGenerator(16);
+        String secret = sgen.generateSecret();
+        setSecret(secret);
+    }
+
+    public User(String username, String password) {
+        this();
+        setUsername(username);
+        setPassword(password);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void addAuthority(Authority authority) {
+        this.authorities.add(authority);
+    }
+
+    public List<TodoList> getTodoLists() {
+        return todoLists;
+    }
+
+    public void setTodoLists(List<TodoList> todoLists) {
+        for (TodoList list : todoLists) {
+            list.setListOwner(this);
+        }
+        this.todoLists = todoLists;
+    }
+}
