3405d5d908343b35d4df5f1a52c84b61b3d839de
Mateusz Bednarski <echelon133@gmail.com>
2018-04-23 15:14:34 +0200

    Added AuthorityRepository JpaRepository

---
+++ src/main/java/ml/echelon133/Repository/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package ml.echelon133.Repository;
+
+import ml.echelon133.Model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+    Authority getAuthorityByAuthorityName(String authorityName);
+}
