ce2f875c254306a7bf8fa160faaf6a33124b735b
Mateusz Bednarski <echelon133@gmail.com>
2018-04-23 11:03:16 +0200

    Added main application class

---
+++ src/main/java/ml/echelon133/TodoAppAPI.java
@@ -0,0 +1,11 @@
+package ml.echelon133;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppAPI {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppAPI.class, args);
+    }
+}
