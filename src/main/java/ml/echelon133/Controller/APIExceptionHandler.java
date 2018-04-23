d5534c7c4e1ffc4f01cabbace84984a68f34bdd4
Mateusz Bednarski <echelon133@gmail.com>
2018-04-23 18:26:17 +0200

    Added APIExceptionHandler with handling of registration exceptions

---
+++ src/main/java/ml/echelon133/Controller/APIExceptionHandler.java
@@ -0,0 +1,38 @@
+package ml.echelon133.Controller;
+
+import ml.echelon133.Exception.RegistrationFailureException;
+import ml.echelon133.Exception.UsernameAlreadyTakenException;
+import ml.echelon133.Model.DTO.APIMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        APIMessage apiMessage = new APIMessage(HttpStatus.BAD_REQUEST);
+        apiMessage.addMessage(ex.getMessage());
+        return new ResponseEntity<>(apiMessage, apiMessage.getHttpStatus());
+    }
+
+    @ExceptionHandler(UsernameAlreadyTakenException.class)
+    protected ResponseEntity<Object> handleUsernameAlreadyTakenException(UsernameAlreadyTakenException ex) {
+        APIMessage apiMessage = new APIMessage(HttpStatus.CONFLICT);
+        apiMessage.addMessage(ex.getMessage());
+        return new ResponseEntity<>(apiMessage, apiMessage.getHttpStatus());
+    }
+
+    @ExceptionHandler(RegistrationFailureException.class)
+    protected ResponseEntity<Object> handleRegistrationFailureException(RegistrationFailureException ex) {
+        APIMessage apiMessage = new APIMessage(HttpStatus.BAD_REQUEST);
+        apiMessage.setMessages(ex.getErrors());
+        return new ResponseEntity<>(apiMessage, apiMessage.getHttpStatus());
+    }
+}
