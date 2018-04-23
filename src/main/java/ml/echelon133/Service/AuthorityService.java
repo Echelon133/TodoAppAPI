8f8cb127b0afd4d5110c94eaaa21665fb2a6cd7b
Mateusz Bednarski <echelon133@gmail.com>
2018-04-23 16:38:42 +0200

    Fixed missing Service annotations

--- src/main/java/ml/echelon133/Service/AuthorityService.java
+++ src/main/java/ml/echelon133/Service/AuthorityService.java
@@ -3,7 +3,9 @@ package ml.echelon133.Service;
 import ml.echelon133.Model.Authority;
 import ml.echelon133.Repository.AuthorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorityService implements IAuthorityService {
 
     @Autowired
--- src/main/java/ml/echelon133/Service/UserService.java
+++ src/main/java/ml/echelon133/Service/UserService.java
@@ -4,7 +4,9 @@ import ml.echelon133.Model.User;
 import ml.echelon133.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService implements IUserService {
 
     @Autowired
