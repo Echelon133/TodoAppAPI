db896bfd0e7907d0391e795729499c11474733a4
Mateusz Bednarski <echelon133@gmail.com>
2018-04-24 14:36:50 +0200

    Implemented successfulAuthentication method in JWTAuthenticationFilter

--- src/main/java/ml/echelon133/Security/JWTAuthenticationFilter.java
+++ src/main/java/ml/echelon133/Security/JWTAuthenticationFilter.java
@@ -51,6 +51,9 @@ public class JWTAuthenticationFilter extends AbstractAuthenticationProcessingFil
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         super.successfulAuthentication(request, response, chain, authResult);
+        if (this.continueChainBeforeSuccessfulAuthentication) {
+            chain.doFilter(request, response);
+        }
     }
 
     @Override
