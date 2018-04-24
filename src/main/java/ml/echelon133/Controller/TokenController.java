22fe22de5b8d30cf6138a6491eb2294fb048a02f
Mateusz Bednarski <echelon133@gmail.com>
2018-04-24 13:41:35 +0200

    Now TokenController works with tokenService and returns jwt tokens when authenticated

--- src/main/java/ml/echelon133/Controller/TokenController.java
+++ src/main/java/ml/echelon133/Controller/TokenController.java
@@ -1,5 +1,7 @@
 package ml.echelon133.Controller;
 
+import ml.echelon133.Service.ITokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,20 @@ import java.security.Principal;
 @RestController
 public class TokenController {
 
+    @Autowired
+    private ITokenService tokenService;
+
     @RequestMapping(value="/users/token", method= RequestMethod.POST)
     public String getToken(Principal principal) {
-        return "TEST_TOKEN";
+        String username = principal.getName();
+
+        String token = tokenService.getTokenOfUser(username);
+
+        if (token.equals("")) {
+            // create a new token and push it to redis
+            token = tokenService.generateTokenForUser(username);
+            tokenService.setTokenOfUser(username, token, 5);
+        }
+        return token;
     }
 }
