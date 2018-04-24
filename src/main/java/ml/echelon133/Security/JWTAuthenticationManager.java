763c4fe6d262079eb507e5786eb3f64ecd41bd56
Mateusz Bednarski <echelon133@gmail.com>
2018-04-24 19:09:29 +0200

    Moved the actual implementation of authenticate to JWTAuthenticationProvider, and JWTAuthenticationManager calls the provider

--- src/main/java/ml/echelon133/Security/JWTAuthenticationManager.java
+++ src/main/java/ml/echelon133/Security/JWTAuthenticationManager.java
@@ -1,41 +1,21 @@
 package ml.echelon133.Security;
 
-import ml.echelon133.Model.User;
-import ml.echelon133.Service.ITokenService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 public class JWTAuthenticationManager implements AuthenticationManager {
 
-    @Autowired
-    private ITokenService tokenService;
+    private AuthenticationProvider authenticationProvider;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    public JWTAuthenticationManager(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        JWTToken jwtToken = (JWTToken)authentication;
-        String token = jwtToken.getToken();
-        String username = tokenService.extractUsernameFromToken(token);
-        if (username == null) {
-            throw new BadCredentialsException("Malformed token");
-        }
-        if (!tokenService.isValidToken(token)) {
-            throw new BadCredentialsException("Token expired or invalid");
-        }
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        Authentication auth = new UsernamePasswordAuthenticationToken(
-                userDetails.getUsername(),
-                token,
-                userDetails.getAuthorities()
-        );
-        return auth;
+        return this.authenticationProvider.authenticate(authentication);
     }
 }
--- src/main/java/ml/echelon133/Security/JWTAuthenticationProvider.java
+++ src/main/java/ml/echelon133/Security/JWTAuthenticationProvider.java
@@ -1,21 +1,41 @@
 package ml.echelon133.Security;
 
-import org.springframework.security.authentication.AuthenticationManager;
+import ml.echelon133.Service.ITokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class JWTAuthenticationProvider implements AuthenticationProvider {
 
-    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ITokenService tokenService;
 
-    public JWTAuthenticationProvider(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return authenticationManager.authenticate(authentication);
+        JWTToken jwtToken = (JWTToken)authentication;
+        String token = jwtToken.getToken();
+        String username = tokenService.extractUsernameFromToken(token);
+        if (username == null) {
+            throw new BadCredentialsException("Malformed token");
+        }
+        if (!tokenService.isValidToken(token)) {
+            throw new BadCredentialsException("Token expired or invalid");
+        }
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                userDetails.getUsername(),
+                token,
+                userDetails.getAuthorities()
+        );
+        return auth;
     }
 
     @Override
