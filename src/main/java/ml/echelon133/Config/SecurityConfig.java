4f5c87f1309581fbe4ae2be3345abd41d8927d1b
Mateusz Bednarski <echelon133@gmail.com>
2018-04-24 19:53:21 +0200

    Written both basic and token-based authentication configs

--- src/main/java/ml/echelon133/Config/SecurityConfig.java
+++ src/main/java/ml/echelon133/Config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package ml.echelon133.Config;
 
+import ml.echelon133.Security.JWTAuthenticationFilter;
+import ml.echelon133.Security.JWTAuthenticationManager;
+import ml.echelon133.Security.JWTAuthenticationProvider;
+import ml.echelon133.Service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -24,27 +33,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public static class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Bean
+        public AuthenticationProvider jwtAuthenticationProvider() {
+            return new JWTAuthenticationProvider();
+        }
+        @Bean
+        public AuthenticationManager jwtAuthenticationManager() {
+            return new JWTAuthenticationManager(jwtAuthenticationProvider());
+        }
+
+        @Bean
         public RequestMatcher jwtRequestMatcher() {
             return new AntPathRequestMatcher("/api/**");
         }
 
+        @Bean
+        public JWTAuthenticationFilter jwtAuthenticationFilter() {
+            JWTAuthenticationFilter filter = new JWTAuthenticationFilter();
+            filter.setAuthenticationManager(jwtAuthenticationManager());
+            filter.setRequiresAuthenticationRequestMatcher(jwtRequestMatcher());
+            filter.setContinueChainBeforeSuccessfulAuthentication(true);
+            return filter;
+        }
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            // Disable CSRF, Http Basic auth and anonymous users for endpoints secured with tokens
-            http.csrf().disable().httpBasic().disable().anonymous().disable();
-            // Use this config only if request matches jwtRequestMatcher
             http
+                    .csrf().disable()
+                    .anonymous().disable()
+                    .httpBasic().disable()
                     .requestMatcher(jwtRequestMatcher())
-                    .authorizeRequests()
-                    .antMatchers(jwtRequestMatcher().toString())
-                    .authenticated()
-                    .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .exceptionHandling();
-
-
+                        .authorizeRequests()
+                            .antMatchers("/api/**").authenticated()
+                        .and()
+                            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .and()
+                            .exceptionHandling()
+                        .and()
+                            .addFilterBefore(jwtAuthenticationFilter(), BasicAuthenticationFilter.class);
         }
     }
 
@@ -57,22 +82,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             return new AntPathRequestMatcher("/users/**");
         }
 
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return new CustomUserDetailsService();
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        }
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
             http
+                    .csrf().disable()
                     .requestMatcher(basicRequestMatcher())
-                    .antMatcher("/users/register")
-                    .anonymous()
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/users/token")
-                    .authenticated()
-                    .and()
-                    .httpBasic()
-                    .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                        .authorizeRequests()
+                            .antMatchers("/users/token").authenticated()
+                            .antMatchers("/users/register").anonymous()
+                        .and()
+                            .httpBasic()
+                        .and()
+                            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .and()
+                            .exceptionHandling();
         }
     }
 }
