89c91a734a622a3744cd9486d00638bbcf9a9008
Mateusz Bednarski <echelon133@gmail.com>
2018-04-25 13:42:53 +0200

    Added ResourceDoesNotExistException

---
+++ src/main/java/ml/echelon133/Exception/ResourceDoesNotExistException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.Exception;
+
+public class ResourceDoesNotExistException extends Exception {
+
+    public ResourceDoesNotExistException(String msg) {
+        super(msg);
+    }
+}
