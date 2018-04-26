dd0eae749ceaf23e2a7c7834c9cf42bc15d1246e
Mateusz Bednarski <echelon133@gmail.com>
2018-04-26 21:42:08 +0200

    Renamed exception that is thrown when validation fails. Now it is called ObjectFailedValidationException

--- src/main/java/ml/echelon133/Exception/ObjectFailedValidationException.java
+++ src/main/java/ml/echelon133/Exception/ObjectFailedValidationException.java
@@ -5,11 +5,11 @@ import org.springframework.validation.FieldError;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TodoListFailedValidationException extends Exception {
+public class ObjectFailedValidationException extends Exception {
 
     private List<FieldError> errors;
 
-    public TodoListFailedValidationException(List<FieldError> errors) {
+    public ObjectFailedValidationException(List<FieldError> errors) {
         this.errors = errors;
     }
 
