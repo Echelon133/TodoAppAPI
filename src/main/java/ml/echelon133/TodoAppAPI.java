29a1b7f69a136d3ad33bf8205e229abc70c1bbf8
Mateusz Bednarski <echelon133@gmail.com>
2018-04-30 11:13:16 +0200

    Updated main app class with IAPIMessage bean definition

--- src/main/java/ml/echelon133/TodoAppAPI.java
+++ src/main/java/ml/echelon133/TodoAppAPI.java
@@ -1,10 +1,24 @@
 package ml.echelon133;
 
+import ml.echelon133.Model.DTO.APIMessage;
+import ml.echelon133.Model.DTO.IAPIMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
+@ComponentScan
 public class TodoAppAPI {
+
+    @Bean
+    @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST)
+    public IAPIMessage apiMessage() {
+        return new APIMessage();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TodoAppAPI.class, args);
     }
