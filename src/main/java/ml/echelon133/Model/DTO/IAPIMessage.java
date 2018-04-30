f5169883816eea579b1393b22d0f2d1653e900ce
Mateusz Bednarski <echelon133@gmail.com>
2018-04-30 11:11:02 +0200

    Extracted an interface out of APIMessage

---
+++ src/main/java/ml/echelon133/Model/DTO/IAPIMessage.java
@@ -0,0 +1,13 @@
+package ml.echelon133.Model.DTO;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public interface IAPIMessage {
+    HttpStatus getHttpStatus();
+    void setHttpStatus(HttpStatus httpStatus);
+    List<String> getMessages();
+    void setMessages(List<String> messages);
+    void addMessage(String message);
+}
